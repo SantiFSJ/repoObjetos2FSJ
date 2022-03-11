8f6a5cc6f649d1987f8921b4c28ea97f4c64bb98
santi <santi@LAPTOP-AB7LDDD1>
2022-03-11 12:11:46 -0300

    1er Commit tp0

--- objetos2tp0/src/objetos2tp0/Main.java
+++ objetos2tp0/src/objetos2tp0/Main.java
@@ -1,8 +1,8 @@
 package objetos2tp0;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Holis");
 	}
 }
---
+++ objetos2tp0/src/objetos2tp0/Tiempo.java
@@ -0,0 +1,13 @@
+package objetos2tp0;
+
+public class Tiempo {
+	int dia;
+	int mes;
+	int año;
+
+	public Tiempo(int dia, int mes, int año) {
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
+	}
+}
